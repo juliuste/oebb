46b32de20a916524349c598c061e0fa92771e6e1
Qinwen Shi <qinwen.shi@gmail.com>
2018-12-22 20:47:58 +0800

    Fix start.js with number of passengers

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const oebb = require('.')
 const moment = require('moment-timezone')
 
 
-const date = moment.tz('Europe/Vienna').add(20, 'days').set('hour', 8).startOf('hour').toDate()
-oebb.journeys('8100108', '8103000', date)
+const date = moment.tz('Europe/Vienna').add(10, 'days').set('hour', 8).startOf('hour').toDate()
+oebb.journeys('8100108', '8103000', date, 2, 1)
   .then(console.log)
   .catch(console.error)
