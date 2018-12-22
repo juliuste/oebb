eb3456f3bf1a840e50d1d11bb85be0f4aa14f6ad
Qinwen Shi <qinwen.shi@gmail.com>
2018-12-22 21:25:14 +0800

    display json stringified instead.

--- start.js
+++ start.js
@@ -5,5 +5,7 @@ const moment = require('moment-timezone')
 
 const date = moment.tz('Europe/Vienna').add(10, 'days').set('hour', 8).startOf('hour').toDate()
 oebb.journeys('8100108', '8103000', date, 2, 1)
-  .then(console.log)
+  .then(function(res){
+    console.log(JSON.stringify(res))
+  })
   .catch(console.error)
